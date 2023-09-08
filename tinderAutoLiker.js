992cb18896d56ae63a8661db7793e1ad3e786dcd
amitoj-singh <31106921+amitoj-singh@users.noreply.github.com>
2023-09-08 20:40:52 +0530

    Created tinderAutoLiker.js

---
+++ tinderAutoLiker.js
@@ -0,0 +1,8 @@
+//
+setInterval(
+  function () {
+    //select right swipe button element and fire a click event
+    likeBtn = document.querySelector("[class='button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style Bxsh($bxsh-btn) Expand Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a']"); 
+    likeBtn.click();
+  }, 5000 //time in miliseconds after which like button will be clicked  
+);
